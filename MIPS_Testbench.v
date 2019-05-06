7afa3c87d4b6385c80c83651638909fbd3bfbebc
logancrow <43042565+logancrow@users.noreply.github.com>
2019-05-06 16:40:43 -0500

    Add files via upload

--- MIPS_Testbench.v
+++ MIPS_Testbench.v
@@ -24,41 +24,38 @@
 module MIPS_Testbench ();
   reg CLK;
   reg RST;
-  wire CS;
-  wire WE;
-  wire [31:0] Mem_Bus;
-  wire [6:0] Address;
+  reg HALT;
+  wire [7:0] r1;
 
   initial
   begin
     CLK = 0;
   end
 
-  MIPS CPU(CLK, RST, CS, WE, Address, Mem_Bus);
-  Memory MEM(CS, WE, CLK, Address, Mem_Bus);
+  Complete_MIPS u1(CLK,RST,HALT,r1);
 
   always
   begin
     #5 CLK = !CLK;
   end
 
-  always
+  initial
   begin
-    RST <= 1'b1; //reset the processor
+    #5 RST = 1'b1; //reset the processor
 
     //Notice that the memory is initialize in the in the memory module not here
 
-    @(posedge CLK);
+
     // driving reset low here puts processor in normal operating mode
-    RST = 1'b0;
+    #5 RST = 1'b0;
+    #5 HALT = 1'b0;
 
     /* add your testing code here */
     // you can add in a 'Halt' signal here as well to test Halt operation
     // you will be verifying your program operation using the
     // waveform viewer and/or self-checking operations
 
-    $display("TEST COMPLETE");
-    $stop;
+
   end
 
 endmodule
